c03832a880c00c355b0dc67ca13191fd4b59fe09
JuleeP <juleeperween34@gmail.com>
2024-04-10 10:58:02 -0700

    super

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,24 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const connectDatabase = require('./db');
 
-// Express app initialize karein
+
+dotenv.config();
+
 const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json());
 // GET request ke liye route define karein
+
 app.get('/', (req, res) => {
   res.send('Welcome to our server!');
 });
 
-// MongoDB connection URL
-const DB_URL = process.env.DB_URL;
-
-// Mongoose connection
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
+connectDatabase()
     
     // Server ko port 3000 par listen karein
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-  });
